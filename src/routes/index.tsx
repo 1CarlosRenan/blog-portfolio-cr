8b9a287baea856fbfb7bdd555ea9db6ebdbc0ec1
1carlosrenan <carlosrenan96@gmail.com>
2023-02-22 21:45:45 -0300

    adding storybook

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,11 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
-import App from '../App'
+import { Routes, Route } from 'react-router-dom'
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path='/' element={<p>teste</p>} />
       <Route path='/teste' element={<p>testando rota</p>} />
+      <Route path='/story' />
 
       {/* <Route path='*' element={<Navigate to='/' />} /> */}
       <Route path='*' element={<p>404</p>} />
